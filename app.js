5675306bbe0696c0c4ca6208f90e378ac3c8114a
herat <herat13198@gmail.com>
2021-02-22 17:20:57 +0530

    demo

--- app.js
+++ app.js
@@ -1,60 +1,58 @@
 var form__insert = document.querySelector('.form__insert');
 var inputbox = document.getElementById('inputbox');
-const todolist = document.querySelector('.todo-list');
+const todolist = document.querySelector('.todo__all-task');
 const poplist = document.querySelector('.poplist');
-const form__filter =document.querySelector('.form__filter');
-const selectcheck =document.querySelector('.select-check');
+const form__filter = document.querySelector('.form__filter');
+const selectcheck = document.querySelector('.select-checkall__filter');
 
 const inputcheckbox = document.getElementById('input-checkbox');
-var tasks=[];
+var tasks = [];
 
-class item{
-    createDiv(itemname){
+class item {
+    createDiv(itemname) {
         let input = document.createElement('input');
-        input.value =itemname;
+        input.value = itemname;
         input.disabled = true;
-        input.classList.add('item_input');
+        input.classList.add('task_input');
         input.type = 'text';
 
-        var index = tasks.length+1;
+        var index = tasks.length + 1;
         //tasks.push({ "name": itemname });
-        tasks.push({ "id":index,"name": itemname });
-        this.setCookie('todolist', tasks,1);
+        tasks.push({ "id": index, "name": itemname });
+        this.setCookie('todolist', tasks, 1);
 
         let itemBox = document.createElement('div');
-        itemBox.classList.add('item');
+        itemBox.classList.add('todo__task');
 
         let id = document.createElement('input');
-        id.value=(index);
-        id.classList.add('item_index');
-        id.hidden=true;
+        id.value = (index);
+        id.classList.add('task_index');
+        id.hidden = true;
 
         let editButton = document.createElement('button');
         editButton.innerHTML = '<i class="fa fa-edit"></i>';
-        editButton.classList.add('button__edit');
-        editButton.classList.add('button__blue');
+        editButton.classList.add('task__edit');
 
         let deleteButton = document.createElement('button');
         deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-        deleteButton.classList.add('button__delete');
-        deleteButton.classList.add('button--red');
+        deleteButton.classList.add('task__delete');
 
         let comButton = document.createElement('button');
         comButton.innerHTML = '<i class="fa fa-check"></i>';
-        comButton.classList.add('button__complate');
-        comButton.classList.add('button--green');
+        comButton.classList.add('task__complate');
 
         let checkboxButton = document.createElement('input');
-        checkboxButton.setAttribute('type','checkbox');
-        checkboxButton.classList.add('button__check');
+        checkboxButton.setAttribute('type', 'checkbox');
+        checkboxButton.classList.add('task__check');
 
         itemBox.appendChild(checkboxButton);
         itemBox.appendChild(input);
+        itemBox.appendChild(id);
         itemBox.appendChild(comButton);
         itemBox.appendChild(editButton);
         itemBox.appendChild(deleteButton);
-        itemBox.appendChild(id);
-              
+
+
         todolist.appendChild(itemBox);
 
         location.reload();
@@ -62,60 +60,58 @@ class item{
     }
 
 
-    edit(item){
-        if(item.classList[0] === "button__edit"){
-            const itemBox =item.parentElement;   
-            let itemindex=itemBox.childNodes[5].value; 
-            this.editcookies(itemBox,itemindex);
+    edit(item) {
+        if (item.classList[0] === "task__edit") {
+            const itemBox = item.parentElement;
+            let itemindex = itemBox.childNodes[5].value;
+            this.editcookies(itemBox, itemindex);
             location.reload();
 
         }
     }
-    
-    remove(item){
-        if(item.classList[0] === "button__delete"){
-            const itemBox =item.parentElement;
-            let itemindex=itemBox.childNodes[5].value;   
-            let val=itemBox.childNodes[1].value;
+
+    remove(item) {
+        if (item.classList[0] === "task__delete") {
+            const itemBox = item.parentElement;
+            let itemindex = itemBox.childNodes[5].value;
+            let val = itemBox.childNodes[1].value;
             itemBox.classList.add('fall');
-            this.removecookies(val,itemindex);
+            this.removecookies(val, itemindex);
             itemBox.addEventListener('transitionend', () => {
                 itemBox.remove();
                 location.reload();
             });
         }
-        
+
 
     }
 
-    complate(item){
-        if(item.classList[0] === "button__complate"){
-            const itemBox =item.parentElement;
-            itemBox.classList.toggle('complated');
+    complate(item) {
+        if (item.classList[0] === "task__complate") {
+            const itemBox = item.parentElement;
+            itemBox.classList.toggle('todo__task--complated');
         }
 
     }
 
-    filtertodo(e){
+    filtertodo(e) {
         const todos = todolist.childNodes;
-        todos.forEach(function(todo){
-            switch(e.target.value)
-            {
+        todos.forEach(function(todo) {
+            switch (e.target.value) {
                 case 'all':
                     todo.style.display = 'flex';
                     break;
                 case 'complated':
-                    if(todo.classList.contains('complated')){
+                    if (todo.classList.contains('todo__task--complated')) {
                         todo.style.display = 'flex';
-                    }
-                    else{
+                    } else {
                         todo.style.display = 'none';
                     }
                     break;
                 case 'uncomplated':
-                    if(!todo.classList.contains('complated')){
+                    if (!todo.classList.contains('todo__task--complated')) {
                         todo.style.display = 'flex';
-                    }else{
+                    } else {
                         todo.style.display = 'none';
                     }
                     break;
@@ -123,8 +119,8 @@ class item{
         });
 
     }
-    
-    
+
+
     setCookie(cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -157,167 +153,159 @@ class item{
             tasks = [];
         }
     }
-    
 
-    removecookies(itemname,itemindex){
+
+    removecookies(itemname, itemindex) {
         tasks = this.getCookie('todolist');
         tasks = JSON.parse(tasks);
         for (let index = 0; index < tasks.length; index++) {
-            if (tasks[index].name==itemname && tasks[index].id==itemindex) {
-                tasks.splice(index,1);
+            if (tasks[index].name == itemname && tasks[index].id == itemindex) {
+                tasks.splice(index, 1);
                 //console.log(tasks);
-                this.setCookie('todolist', tasks,1);
-            }   
+                this.setCookie('todolist', tasks, 1);
+            }
         }
     }
 
-    editcookies(itemBox,itemindex){
-        let val=itemBox.childNodes[1].value;
+    editcookies(itemBox, itemindex) {
+        let val = itemBox.childNodes[1].value;
         console.log(val);
-        let user=prompt("Enter : ",val);
+        let user = prompt("Enter : ", val);
         tasks = this.getCookie('todolist');
         tasks = JSON.parse(tasks);
         for (let index = 0; index < tasks.length; index++) {
-            if (tasks[index].name==val && tasks[index].id==itemindex) {
-                tasks.splice(index,1,{id:itemindex, name:user});
-                this.setCookie('todolist', tasks,1);
+            if (tasks[index].name == val && tasks[index].id == itemindex) {
+                tasks.splice(index, 1, { id: itemindex, name: user });
+                this.setCookie('todolist', tasks, 1);
             }
-            
+
         }
     }
 
     showCookie(cname) {
         tasks = this.getCookie('todolist');
         tasks = JSON.parse(tasks);
-        let index = tasks.length-1;
-        
-        while(index>=0){
+        let index = tasks.length - 1;
+
+        while (index >= 0) {
             let input = document.createElement('input');
-            input.value =(tasks[index].name);
+            input.value = (tasks[index].name);
             input.disabled = true;
-            input.classList.add('item_input');
+            input.classList.add('task__input');
             input.type = 'text';
-        
+
             let itemBox = document.createElement('div');
-            itemBox.classList.add('item');
-        
+            itemBox.classList.add('todo__task');
+
             let id = document.createElement('input');
-            id.value=(tasks[index].id);
-            id.classList.add('item_index');
-            id.hidden=true;
-    
+            id.value = (tasks[index].id);
+            id.classList.add('task_index');
+            id.hidden = true;
+
             let editButton = document.createElement('button');
             editButton.innerHTML = '<i class="fa fa-edit"></i>';
-            editButton.classList.add('button__edit');
-            editButton.classList.add('button--blue');
-        
+            editButton.classList.add('task__edit');
+
             let deleteButton = document.createElement('button');
             deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-            deleteButton.classList.add('button__delete');
-            deleteButton.classList.add('button--red');
-        
+            deleteButton.classList.add('task__delete');
+
             let comButton = document.createElement('button');
             comButton.innerHTML = '<i class="fa fa-check"></i>';
-            comButton.classList.add('button__complate');
-            comButton.classList.add('button--green');
-                
+            comButton.classList.add('task__complate');
+
             let checkboxButton = document.createElement('input');
-            checkboxButton.setAttribute('type','checkbox');
-            checkboxButton.classList.add('button__check');
-                
+            checkboxButton.setAttribute('type', 'checkbox');
+            checkboxButton.classList.add('task__check');
+
             itemBox.appendChild(checkboxButton);
             itemBox.appendChild(input);
+            itemBox.appendChild(id);
             itemBox.appendChild(comButton);
             itemBox.appendChild(editButton);
             itemBox.appendChild(deleteButton);
-            itemBox.appendChild(id);
-                
-                
+
+
+
             todolist.appendChild(itemBox);
-    
-            comButton.addEventListener('click', ()=> this.complate(comButton));
-            editButton.addEventListener('click', ()=> this.edit(editButton));
-            deleteButton.addEventListener('click', ()=> this.remove(deleteButton));
-            form__filter.addEventListener('change',()=> this.filtertodo(event));
-            checkboxButton.addEventListener('click', ()=> this.checkselectAll(todolist));        
-            selectcheck.addEventListener('change', ()=> this.checked(selectcheck));
-            inputcheckbox.addEventListener('click', ()=> this.SelectAll(inputcheckbox));
-            
+
+            comButton.addEventListener('click', () => this.complate(comButton));
+            editButton.addEventListener('click', () => this.edit(editButton));
+            deleteButton.addEventListener('click', () => this.remove(deleteButton));
+            form__filter.addEventListener('change', () => this.filtertodo(event));
+            checkboxButton.addEventListener('click', () => this.checkselectAll(todolist));
+            selectcheck.addEventListener('change', () => this.checked(selectcheck));
+            inputcheckbox.addEventListener('click', () => this.SelectAll(inputcheckbox));
+
             index--;
         }
     }
-   
-    checked(item){
+
+    checked(item) {
         let cmtbybox = todolist.childNodes;
-        switch(item.value){
+        switch (item.value) {
             case 'delete':
-                cmtbybox.forEach(item=>{
-                    if(item.childNodes[0].checked){
-                        let val=item.childNodes[1].value;
-                        let itemindex=item.childNodes[5].value;
+                cmtbybox.forEach(item => {
+                    if (item.childNodes[0].checked) {
+                        let val = item.childNodes[1].value;
+                        let itemindex = item.childNodes[5].value;
                         item.classList.add("fall");
-                        inputcheckbox.checked=false;
-                        this.removecookies(val,itemindex);
-                        item.addEventListener('transitionend',function(){
-                        item.remove();
-                        location.reload();
-                    });
-                }
-                selectcheck.value="";
-            }, this);
-            break;
+                        inputcheckbox.checked = false;
+                        this.removecookies(val, itemindex);
+                        item.addEventListener('transitionend', function() {
+                            item.remove();
+                            location.reload();
+                        });
+                    }
+                    selectcheck.value = "";
+                }, this);
+                break;
             case 'completed':
-                cmtbybox.forEach(item=>{
-                    if(item.childNodes[0].checked)
-                    {
-                        if (item.classList.contains("complated")) {
+                cmtbybox.forEach(item => {
+                    if (item.childNodes[0].checked) {
+                        if (item.classList.contains("todo__task--complated")) {
                             item.children[0].checked = false;
-                            inputcheckbox.checked=false;
-                        }
-                        else{
-                            item.classList.toggle("complated");
+                            inputcheckbox.checked = false;
+                        } else {
+                            item.classList.toggle("todo__task--complated");
                             item.children[0].checked = false;
-                            inputcheckbox.checked=false;
+                            inputcheckbox.checked = false;
                         }
                     }
-                    selectcheck.value="";
+                    selectcheck.value = "";
                 });
-            break;    
+                break;
             case 'uncomplated':
-                cmtbybox.forEach(item=>{
-                    if(item.childNodes[0].checked)
-                    {
-                        if (!item.classList.contains("complated")) {
+                cmtbybox.forEach(item => {
+                    if (item.childNodes[0].checked) {
+                        if (!item.classList.contains("todo__task--complated")) {
                             item.children[0].checked = false;
-                            inputcheckbox.checked=false;
+                            inputcheckbox.checked = false;
+                        } else {
+                            item.classList.toggle("todo__task--complated");
+                            item.children[0].checked = false;
+                            inputcheckbox.checked = false;
                         }
-                        else{
-                            item.classList.toggle("complated");
-                            item.children[0].checked = false;
-                            inputcheckbox.checked=false;
-                        }   
                     }
-                    selectcheck.value="";
+                    selectcheck.value = "";
                 });
-            break;
+                break;
         }
     }
 
-    SelectAll(item){
+    SelectAll(item) {
         let selectbox = todolist.childNodes;
-        if(selectbox.length === 0)
-        {
+        if (selectbox.length === 0) {
             item.checked = false
             alert("No Data Available...");
-        }
-        else{
-            if(item.checked){
-                selectbox.forEach(item=>{
+        } else {
+            if (item.checked) {
+                selectbox.forEach(item => {
                     item.children[0].checked = true;
                 })
             }
-            if(item.checked === false){
-                selectbox.forEach(item=>{
+            if (item.checked === false) {
+                selectbox.forEach(item => {
                     item.children[0].checked = false;
                 })
             }
@@ -325,18 +313,18 @@ class item{
 
     }
 
-    checkselectAll(item){
+    checkselectAll(item) {
         let selectbox = item.childNodes;
-        let count=0;
-        selectbox.forEach(item=>{
-            if (item.children[0].checked === true){
+        let count = 0;
+        selectbox.forEach(item => {
+            if (item.children[0].checked === true) {
                 count++
             }
         });
-        if(selectbox.length==count){
-            inputcheckbox.checked=true;
-        }else{
-            inputcheckbox.checked=false;
+        if (selectbox.length == count) {
+            inputcheckbox.checked = true;
+        } else {
+            inputcheckbox.checked = false;
         }
     }
 
@@ -345,12 +333,12 @@ class item{
 //new item().init();
 document.addEventListener("DOMContentLoaded", new item().init());
 document.addEventListener("DOMContentLoaded", new item().showCookie('todolist'));
-function check(event){
-    if(inputbox.value === ""){
+
+function check(event) {
+    if (inputbox.value === "") {
         event.preventDefault();
         alert("Please enter your name ...");
-    }
-    else{
+    } else {
         event.preventDefault();
         new item().createDiv(inputbox.value);
         alert("Add done ...");
